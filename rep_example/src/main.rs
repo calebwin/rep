f512afc1606d60d9a49d3e4a7794011cc4f6137a
Caleb Winston <calebhwin@gmail.com>
2019-11-04 13:04:23 -0800

    added details to Cargo.toml

--- rep_example/src/main.rs
+++ rep_example/src/main.rs
@@ -2,18 +2,16 @@ use rep::*;
 
 #[derive(CheckRep)]
 struct Line {
-	// assert equality
-	// assert relation
-	// #[rep(aseert = "not_eq_zero")]
-	// #[rep(assert_false = "eq_zero")]
+	// #[rep(assert_default)]
+	// #[rep(assert_true)]
+	// #[rep(assert_false)]
 	// #[rep(assert_eq = 0.0]
 	// #[rep(assert_eq = true]
-	// #[rep(assert)]	
 	// #[rep(assert_eq = "hello")]
 	// #[rep(assert_gt = 0.0]
-	// #[rep(assert_lt = 0.0]
+	// #[rep(assert_lt = 10.0]
 	// #[rep(assert_ge = 0.0]
-	// #[rep(assert_le = 0.0]
+	// #[rep(assert_le = 10.0]
 	#[rep(assert_gt = 0)]
 	x1: i32,
 	y1: i32,
@@ -23,7 +21,7 @@ struct Line {
 
 #[check_rep]
 impl Line {
-	pub fn foo() {
+	pub fn foo(&mut self) {
 	}
 }
 
@@ -35,6 +33,6 @@ fn main() {
 		y2: 10
 	};
 
-	// line.foo();
-	Line::foo();
+	line.foo();
+	// Line::foo();
 }
